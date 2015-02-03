44fee79c8e155aca0cb8b629e29bbd64c976e62b
Ryan Newton <rnewton@hearsaycorp.com>
2015-02-02 20:24:59 -0800

    Initial Commit

---
+++ RottenTomatoes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RottenTomatoes
+//
+//  Created by Ryan Newton on 2/2/15.
+//  Copyright (c) 2015 ___rvkn___. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
